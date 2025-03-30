3832469f3d0aa1f842f93fa821adbedab10aee86
Adjei-Kuranchie <yaw.kuranchie@stu.ucc.edu.gh>
2025-03-30 23:03:51 +0000

    update root route to display Jobs API homepage and add documentation link

--- app.js
+++ app.js
@@ -38,8 +38,10 @@ app.use(
 app.use(helmet());
 app.use(cors());
 app.use(xss());
-
-app.get("/", swaggerUI.serve, swaggerUI.setup(swaggerDocument));
+app.get("/", (req, res) => {
+  res.send(`<h1>Jobs API</h1><a href="/api-use">Documentation</a>`);
+});
+app.get("/api-use", swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 // routes
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/jobs", authenticateUser, jobsRouter);
